4dee756e3ac65ed830229677f803f2cefb89f303
KajalTomar <kajaltomar09@gmail.com>
2021-01-12 12:12:16 -0600

    testing

--- test.py
+++ test.py
@@ -15,14 +15,16 @@ cur = conn.cursor()
 
 today = datetime.today()
 now = datetime.now()
-currentTime = now.strftime("%H:%M:%S")
+
 currentDate = today.strftime("%Y-%m-%d")
+currentTime = now.strftime("%H:%M:%S")
+
 streak = 5
 
 SQL = "INSERT INTO streakinfo(date,streak,time)values(%s,%s,%s)"
 
 #cur.execute("CREATE TABLE streakinfo (date date PRIMARY KEY, streak smallint, time time);")
-cur.execute("INSERT INTO streakinfo(date,streak,time)values(%s,%s,%s)",(today,streak,now))
+cur.execute("INSERT INTO streakinfo(date,streak,time)values(%s,%s,%s)",(currentDate,streak,currentTime))
 
 conn.commit()
 
